2473620a6a12f5da03afd65373841a829071faf3
Victor Lovera <victor260622@gmail.com>
2025-12-13 22:20:55 -0300

    BREAKING CHANGE: adding atomic desing structural folders and tailwidn css

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './app.vue',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+} satisfies Config
