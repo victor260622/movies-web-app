ef44e14bf216cda62df1b7ed9db02f82de3c61db
Victor Lovera <victor260622@gmail.com>
2025-12-18 01:38:48 -0300

    refactor: eliminating unused code

--- server/api/[imdbId].get.ts
+++ server/api/[imdbId].get.ts
@@ -15,18 +15,3 @@ export default defineEventHandler(async (event) => {
     return { error: "No se pudo obtener detalles de la película" };
   }
 });
-/* export default defineEventHandler(async (event) => {
-  const imdbId = getRouterParam(event, "imdbId")
-  const config = useRuntimeConfig()
-
-  if (!imdbId) return { error: "No se recibió un imdbId válido" }
-
-  try {
-    const res = await $fetch(`http://www.omdbapi.com/?i=${imdbId}&apikey=${config.OMDB_API_KEY}`)
-    const res = await $fetch(`http://www.omdbapi.com/?i=tt2709768&apikey=601c5309`)
-    return res
-  } catch {
-    return { error: "No se pudo obtener detalles de la película" }
-  }
-})
- */
